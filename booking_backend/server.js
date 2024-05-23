e130a6413b9be4207b8e5893e5d923be0e7c1624
Duyb2014733 <ldduy038@gmail.com>
2024-05-23 14:01:13 +0700

    update

--- booking_backend/server.js
+++ booking_backend/server.js
@@ -1,19 +1,22 @@
 const app = require("./app");
 const config = require("./app/config");
-const MongoDB = require("./app/utils/mongodb.util");
+const mongoose = require("mongoose");
 
 async function startServer() {
   try {
-    await MongoDB.connect(config.db.uri);
-    console.log("Connect to the database!");
+    await mongoose.connect(config.db.uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to the database!");
 
     const PORT = config.app.port;
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
-    console.log("Cannot connect to the database!", error);
-    process.exit();
+    console.error("Cannot connect to the database!", error);
+    process.exit(1);
   }
 }
 
